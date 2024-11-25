d0cb0d5d31ed689b825a91b40df6185f7c8223e6
hadiz <nana-hadiza.BACHIR-ABDOU@etu.univ-amu.fr>
2024-11-25 08:26:42 +0100

    Mise à jour ModelElement et ViewElement routes

--- src/main/java/view/ViewElement.java
+++ src/main/java/view/ViewElement.java
@@ -3,7 +3,7 @@ package view;
 import javafx.scene.paint.Color;
 
 public enum ViewElement {
-  FIREFIGHTER(Color.BLUE), FIRE(Color.RED), EMPTY(Color.WHITE), CLOUD(Color.GRAY), MOTORIZED_FIREFIGHTER(Color.BLACK), MOUNTAIN(Color.GREEN);
+  FIREFIGHTER(Color.BLUE), FIRE(Color.RED), EMPTY(Color.WHITE), CLOUD(Color.GRAY), MOTORIZED_FIREFIGHTER(Color.BLACK), MOUNTAIN(Color.GREEN), ROUTE(Color.BROWN);
   final Color color;
   ViewElement(Color color) {
     this.color = color;
