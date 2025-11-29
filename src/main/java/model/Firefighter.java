0976a7ad475a3a9b4e63e6463c4648dc919fb413
Faiza <faiza.siroukane@etu.univ-amu.fr>
2025-11-29 18:46:54 +0100

    le déplacement vers le feu et l'extinction des cellules enflammées

--- src/main/java/model/Firefighter.java
+++ src/main/java/model/Firefighter.java
@@ -3,23 +3,53 @@ import util.Position;
 import util.TargetStrategy;
 import java.util.*;
 
-public class Firefighter {
-    private Position position; //la position actuel de pompier
-    private TargetStrategy strategy = new TargetStrategy(); //le voisin le plus proche  au feu
+public class Firefighter implements Element {
+    public Position position; //la position actuel de pompier
+    public TargetStrategy strategy = new TargetStrategy(); //le voisin le plus proche  au feu
+    public Map<Position, Cell> terrain;
 
 
     public Firefighter(Position initialPosition) {
         this.position = initialPosition;
     }
 
-    public Position getPosition() {
-        return position;
-    }
+    /*public Position move(Set<Position> firePositions, Map<Position, List<Position>> neighbors) {
+        Position newPosition = strategy.neighborClosestToFire(position,firePositions,neighbors);
+        if(terrain != null) {
+            Cell target = terrain.get(newPosition);
+            if(!target.canEntityMove()) return position;
+        }
+
+
+    position = newPosition;
+    return position;
+}*/
 
     public Position move(Set<Position> firePositions, Map<Position, List<Position>> neighbors) {
-        position = strategy.neighborClosestToFire(position, firePositions, neighbors);
-        return position;
+
+
+        List<Position> available = new ArrayList<>();
+        for (Position p : neighbors.get(position)) {
+            Cell c = terrain.get(p);
+
+            if (c == null || c.canEntityMove()) {
+                available.add(p);
+            }
+        }
+
+        if (available.isEmpty()) {
+            return position;
+        }
+
+        Position next = strategy.neighborClosestToFire(position, firePositions, neighbors);
+
+        position = next;
+        return next;
     }
+
+
+
+
 
 
     public List<Position> extinguish(Fire fire, Map<Position, List<Position>> neighbors) {
@@ -31,10 +61,13 @@ public class Firefighter {
             extinguished.add(position);
         }
 
-        for (Position neighbor : neighbors.get(position)) {
-            if (fire.isOnFire(neighbor)) {
-                fire.extinguish(neighbor);
-                extinguished.add(neighbor);
+        List<Position> voisins = neighbors.get(position);
+        if (voisins != null) {
+            for (Position neighbor : voisins) {
+                if (fire.isOnFire(neighbor)) {
+                    fire.extinguish(neighbor);
+                    extinguished.add(neighbor);
+                }
             }
         }
 
@@ -43,5 +76,22 @@ public class Firefighter {
 
 
 
+    @Override
+    public Position getPosition() {
+        return position;
+    }
 
-}
+
+    @Override
+    public void update(FirefighterBoard board) {
+
+        if (terrain == null) {
+            terrain = board.getTerrain();
+        }
+        // Se déplacer
+        move(board.getFire().getPositions(), board.getNeighbors());
+        // Éteindre les feux
+        extinguish(board.getFire(), board.getNeighbors());
+    }
+
+    }
