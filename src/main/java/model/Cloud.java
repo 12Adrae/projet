adcc928137e7b3f3f93e7f4ff0bc264f537f1f77
Faiza <faiza.siroukane@etu.univ-amu.fr>
2025-12-08 16:57:23 +0100

    classe Cloud permettant un déplacement aléatoire et l'extinction passive du feu

--- src/main/java/model/Cloud.java
+++ src/main/java/model/Cloud.java
@@ -1,56 +1,64 @@
 package model;
-
 import util.Position;
 import java.util.*;
 
 
 public class Cloud implements Element {
-
     private Position position;
     private final Random random = new Random();
 
-
-    public Cloud(Position position) {
-        this.position = position;
+    public Cloud(Position start) {
+        this.position = start;
     }
 
+    @Override
+    public Set<Position> getPositions() {
+        return Set.of(position);
+    }
 
     @Override
-    public Position getPosition() {
+    public ModelElement getType() {
+        return ModelElement.CLOUD;
+    }
+
+    private Position move(Map<Position, List<Position>> neighbors) {
+        List<Position> possibles = neighbors.get(position);
+
+        if (!possibles.isEmpty()) {
+            position = possibles.get(random.nextInt(possibles.size()));
+        }
+
         return position;
     }
 
-
-    public void moveRandomly(Map<Position, List<Position>> neighbors) {
-        List<Position> possibleMoves = neighbors.get(position);
-
-        // Vérifications
-        if (possibleMoves == null || possibleMoves.isEmpty()) {
-            return;
-        }
-
-        // Choisir un voisin aléatoire
-        int randomIndex = random.nextInt(possibleMoves.size());
-        Position newPosition = possibleMoves.get(randomIndex);
-
-        if (newPosition != null) {
-            this.position = newPosition;
-        }
-    }
-
-
-    public void extinguishFire(Fire fire) {
-        if (fire != null) {
-            fire.extinguish(position);
-        }
-    }
-
     @Override
-    public void update(FirefighterBoard board) {
-        // Se déplacer aléatoirement
-        moveRandomly(board.getNeighbors());
+    public List<Position> update(Map<Position, List<Position>> neighbors,
+                                 FirefighterBoard board) {
 
-        // Éteindre les feux
-        extinguishFire(board.getFire());
+        List<Position> modified = new ArrayList<>();
+        Position oldPos = position;
+
+        // déplacement
+        Position newPos = move(neighbors);
+
+        modified.add(oldPos);
+        modified.add(newPos);
+
+        // chercher l'élément Fire dans la liste des Element
+        Fire fire = null;
+        for (Element e : board.getElements()) {
+            if (e instanceof Fire f) {
+                fire = f;
+                break;
+            }
+        }
+
+        // extinction
+        if (fire != null) {
+            fire.extinguish(newPos);
+            modified.add(newPos);
+        }
+
+        return modified;
     }
 }
