033eb3ad330841f085c049f7b26ebde7d4b3b25e
Faiza <faiza.siroukane@etu.univ-amu.fr>
2025-12-08 16:58:08 +0100

    Interface pour tous les éléments, Permet au FirefighterBoard de traiter uniformément tous les éléments

--- src/main/java/model/Element.java
+++ src/main/java/model/Element.java
@@ -1,10 +1,14 @@
 package model;
-
 import util.Position;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 
 public interface Element {
+    Set<Position> getPositions();
+    List<Position> update(Map<Position, List<Position>> neighbors, FirefighterBoard board);
+    ModelElement getType();
 
-    void update(FirefighterBoard board);
-    Position getPosition();
+
 }
